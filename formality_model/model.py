388feb58f08bd83b3f703219e18644d135f7fb6e
Brian <brdiep113@gmail.com>
2022-10-13 15:00:14 -0400

    Add rescaling + stratification

--- formality_model/model.py
+++ formality_model/model.py
@@ -1,16 +1,19 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 import statsmodels.api as sm
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.metrics import r2_score
 import tqdm
 
 
 class Config:
     """Config class used to store hyperparameters for models."""
-    def __init__(self, epochs, k, seed):
+    def __init__(self, epochs, k, stratified, bins, seed):
         self.epochs = epochs
         self.k = k
+        self.stratified = stratified
+        self.bins = bins
         self.seed = seed
 
 class SBERT:
@@ -30,16 +33,38 @@ class BetaRegression:
         self.config = config
         self.best_model = None
         self.best_fit = -1
+        self.history = {}
+
     
+    def get_score_groups(self, number_of_groups) -> None:
+        """Split score into specified bins for stratification."""
+        intervals = pd.cut(self.scores, bins=number_of_groups)
+
+        return intervals
+    
+    def get_kfolds(self):
+        """Return appropriate KFold generator based on config."""
+        if self.config.stratified:
+            kf = StratifiedKFold(n_splits=self.config.k,
+                                 random_state=self.config.seed,
+                                 shuffle=True)
+        else:
+            kf = KFold(n_splits=self.config.k,
+                       random_state=self.config.seed,
+                       shuffle=True)
+        
+        return kf
+
     def train_model(self):
-        kf = KFold(n_splits=self.config.k,
-                   random_state=self.config.seed,
-                   shuffle=True)
+
+        kf = self.get_kfolds()
 
         curr_best_fit = -1
         curr_best_model = None
 
-        for train_idx, test_idx in kf.split(X=self.scores):
+        score_groups = self.get_score_groups(self.config.bins)
+
+        for k, (train_idx, test_idx) in enumerate(kf.split(X=self.scores, y=score_groups)):
             train, test = self.embeddings, self.scores
             X_train = train[train_idx]
             y_train = test.iloc[train_idx, :]
@@ -50,6 +75,9 @@ class BetaRegression:
             binom_glm = sm.GLM(y_train, X_train, family=sm.families.Binomial())
             binom_fit_model = binom_glm.fit()
             fit_val = self.goodness_of_fit(binom_fit_model, y_test, X_test)
+
+            self.history[k] = fit_val
+            self.plot_predictions(binom_fit_model, y_test, X_test, k)
 
             if fit_val > curr_best_fit:
                 print("NEW BEST MODEL")
@@ -63,9 +91,36 @@ class BetaRegression:
         return curr_best_model
 
     
-    def get_goodness_of_fit(model, y_true, X):
+    def get_goodness_of_fit(self, model, y_true, X):
         """Return R^2 value of model to compare goodness of fit."""
         y_predicted = model.get_prediction(X)
-        pred_vals = y_predicted.summary_frame()['mean']
+        pred_vals = y_predicted.summary_frame()["mean"]
         fit_val = r2_score(y_true, pred_vals)
         return fit_val
+
+    
+    def plot_predictions(self, model, y_true, X, k):
+        """Plot model formality predictions vs. actual predictions."""
+        y_predicted = model.get_prediction(X)
+        pred_vals = y_predicted.summary_frame()["mean"]
+
+        # Unscale values before plotting
+        pred_vals = pred_vals * (6) + 3
+        y_true = y_true * 6 + 3
+        
+
+        plt.scatter(y_true, pred_vals, alpha=0.8)
+        plt.xlabel("Actual formality score")
+        plt.ylabel("Predicted formality score")
+        plt.title(f"{k}-folds Formality model predictions R^2 {self.history[k]}")
+
+        plt.savefig(f"figs/{k}_formality_model_predictions.png")
+
+
+    # TO DO: Get sentence associated with test set and their predictions
+    def get_sentences(self, model, y_true, X, text):
+        """Create DataFrame of sentences and their predictions."""
+        y_predicted = model.get_prediction(X)
+        pred_vals = y_predicted.summary_frame()["mean"]
+
+        pass
--- formality_model/preprocess.py
+++ formality_model/preprocess.py
@@ -9,20 +9,35 @@ def load_formality_data(path_to_data):
     Returns:
         DataFrame: Pandas DataFrame containing formality data.
     """
+
+    # Read data
     df = pd.read_csv(path_to_data, sep="\t", encoding="utf-8")
+
+    # Keep relevant columns
     df = df.iloc[:, [0, 3]]
     df.columns = ["avg_formality", "text"]
+    
+    # Minmax rescale between 0 and 1
+    df["avg_formality"] = rescale_formality(df["avg_formality"])
+
     return df
 
+
+def rescale_formality(col):
+    """Rescale a column between 0 and 1 for BetaRegression."""
+    rescaled = (col - col.min()) / (col.max() - col.min())
+    return rescaled
+
 def formality_dataset_generator():
+    # Read each type of data
     datasets = ["answers", "blog", "email", "news"]
     for dataset in datasets:
         yield load_formality_data(f"data/data-for-release/{dataset}")
 
 def load_formality_dataset_to_df():
+    # Stitch data into DataFrame
     df = pd.concat(list(formality_dataset_generator()), ignore_index=True)
     return df
 
 if __name__ == "__main__":
     df = load_formality_dataset_to_df()
-    
